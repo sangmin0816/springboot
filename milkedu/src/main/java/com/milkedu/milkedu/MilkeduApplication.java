122e73392023247f193fa9e99ab67c3484761cf4
sangmin0816 <sangmin990816@gmail.com>
2023-11-08 23:40:22 +0900

    spring boot personal project

---
+++ milkedu/src/main/java/com/milkedu/milkedu/MilkeduApplication.java
@@ -0,0 +1,13 @@
+package com.milkedu.milkedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkeduApplication.class, args);
+	}
+
+}
---
+++ milkedu/src/test/java/com/milkedu/milkedu/MilkeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milkedu.milkedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
